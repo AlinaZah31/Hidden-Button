e2a1e94f221cf0a540ca5ada060439de86a662b7
AlinaZah31 <alina_shuvi@yahoo.com>
2024-06-04 22:44:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const results = ["1", "2", "3"];
-const random = Math.floor(Math.random() * results.length);
+const randomPos = Math.floor(Math.random() * results.length);
+const randomBtn = results[randomPos];
+const first = document.getElementById("btn1").innerHTML;
+const second = document.getElementById("btn2").innerHTML;
+const third = document.getElementById("btn3").innerHTML;
 
-function displayMessage() {
-    const idBtn = document.getElementById("btn").innerHTML;
-    if (idBtn == "btn" + results[random]) {
+function displayMessage(buttonId, randomBtn) {
+    if (buttonId == "Button " + randomBtn) {
         document.getElementById("Initial-text").innerHTML="You win!";
     } else {
         document.getElementById("Initial-text").innerHTML= "You lose!";
