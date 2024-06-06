09a68fe3f64ab3a52307f5b482a4b7f8ac4b9df8
AlinaZah31 <alina_shuvi@yahoo.com>
2024-06-06 22:39:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const randomBtn = Math.floor(Math.random() * ["1", "2", "3"].length) + 1; // +1, because it will save the position from the array, meaning 0, 1 or 2
+const randomBtn = Math.floor(Math.random() * 3) + 1; // +1, because it will save the position from the array, meaning 0, 1 or 2
 
 function displayMessage(buttonId) {
     if (buttonId == randomBtn) {
