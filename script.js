27521f3378669222e6e5aad6f0c15baaac05a663
AlinaZah31 <alina_shuvi@yahoo.com>
2024-06-07 14:37:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const randomBtn = Math.floor(Math.random() * 3) + 1; // +1, because it will save the position from the array, meaning 0, 1 or 2
 
 function displayMessage(buttonId) {
-    if (buttonId == randomBtn) {
-        document.getElementById("Initial-text").innerHTML="You win!";
+    if (buttonId === randomBtn) {
+        document.getElementById("Initial-text").innerHTML = "You win!";
     } else {
-        document.getElementById("Initial-text").innerHTML= "You lose!";
+        document.getElementById("Initial-text").innerHTML = "You lose!";
     }
 }
